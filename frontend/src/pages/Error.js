9a7c852cdc331f71545a33ceca96fd122a1a0edf
VivienIngrams <vivingrams@gmail.com>
2023-05-31 16:10:53 +0100

    Using errorElement and json from react router dom

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -1,5 +1,31 @@
+import { useRouteError } from "react-router-dom";
+
+import MainNavigation from "../components/MainNavigation";
+import PageContent from "../components/PageContent";
+
 function Error() {
-    return <h1>An Error Ocurred</h1>
-};
+  const error = useRouteError();
+
+  let title = "Something went wrong...";
+  let message = "An error occurred";
+
+  if (error.status === 500) {
+    message = error.data.message;
+  }
+
+  if (error.status === 404) {
+    title = "Not found!";
+    message = "Could not find resource or page.";
+  }
+
+  return (
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>{" "}
+    </>
+  );
+}
 
 export default Error;
